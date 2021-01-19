a017334a93bf220807912bd854b47242de025cdd
AfdolCED9 <s6302041620176@email.kmutnb.ac.th>
2021-01-19 14:18:15 +0700

    update program register

---
+++ variable.c
@@ -0,0 +1,36 @@
+#iclude <stdio.h>
+//  ส่วน ดึง Library ของโปรแกรม
+
+void main(){
+// ส่วน การสร้าง Funtion หลัก
+
+// =================== ตัวแปร ===================
+// ทำระบบสมัครสมาชิก เก็บ username, email, password, phone
+//                      char[]   char[] char[]    char[]
+
+char username, email, password, repassword;
+int phone;
+
+// =================== ตัวแปร ===================
+
+// =================== การรับข้อมูล ===================
+printf("You username is :");
+scanf("%c",&username);
+
+printf("You email is :");
+scanf("%c",&email);
+
+printf("You password is :");
+scanf("%c",&password);
+
+printf("You repassword is :");
+scanf("%c",&repassword);
+
+printf("You phone is :");
+scanf("%d",&phone);
+
+// =================== การรับข้อมูล ===================
+
+// =================== การแสดงผลข้อมูล ===================
+
+}
