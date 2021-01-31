d3241b5abe9d7d34db82334d0f81862c8aa164d4
AfdolCED9 <s6302041620176@email.kmutnb.ac.th>
2021-01-31 14:33:52 +0700

    re update program

---
+++ Easywork2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+// Import Libraly
+
+void main(){
+
+    int std1_match, std2_programming, std3_science;
+
+    printf("***********************************\n");
+    printf("Please input score\n");
+    printf("Subject-> 1.Match 2.Programming 3.Science\n");
+    printf("Student 1 : ");
+    scanf("%d %d %d\n",&std1_match,&std2_programming,&std3_science);
+
+    printf("Score match %d program %d science %d",std1_match ,std2_programming ,std3_science);
+
+    return 0 ;
+}
