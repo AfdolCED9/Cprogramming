2a3d6c82c40c96cbda234942324e0d36feeff496
AfdolCED9 <s6302041620176@email.kmutnb.ac.th>
2021-01-31 14:36:40 +0700

    aa

--- Easywork2.c
+++ Easywork2.c
@@ -9,7 +9,7 @@ void main(){
     printf("Please input score\n");
     printf("Subject-> 1.Match 2.Programming 3.Science\n");
     printf("Student 1 : ");
-    scanf("%d %d %d\n",&std1_match,&std2_programming,&std3_science);
+    scanf("%d %d %d\n", &std1_match, &std2_programming, &std3_science);
 
     printf("Score match %d program %d science %d",std1_match ,std2_programming ,std3_science);
 
